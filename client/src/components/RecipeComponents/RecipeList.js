a0dee1b4015bf320d4761e602c3d5e01be91385e
Alex De Michieli <alexdemichieli@Alexs-MacBook-Pro.local>
2023-02-22 11:45:16 -0800

    removed strict mode

--- client/src/components/RecipeComponents/RecipeList.js
+++ client/src/components/RecipeComponents/RecipeList.js
@@ -1,6 +1,5 @@
 import {useEffect, useState } from "react";
 import client from "../../utils/client";
-import { UserContext } from '../../utils/setContext';
 import RecipeCard from "./RecipeCard";
 import Search from "./Search";
 
@@ -31,7 +30,6 @@ const RecipeList = () => {
             try {
                 const response = await client.get("/api/get_recipes");
                 setRecipes(response.data);
-                console.log(response.data)
             } catch (error) {
                 console.error(error);
             }
